687749ac52b014519eb8920997adeddcbc8fa59d
Caleb Wallace <wallacekid1996@gmail.com>
2022-11-23 16:50:42 -0700

    removed random stuff

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require('express')
-// const cors = require('cors')
+
 const app = express()
 
 app.use(express.json())
-// app.use(cors())
+
 
 app.use(express.static(`${__dirname}/public`))
 
-app.get(`/api/cat`, (req, res) => {
-    res.status(200).send(`Macy`)
-})
 
 app.listen(4001,
    () => console.log(`server running on 4001`)
