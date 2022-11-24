3a4d8f59f9e73f238f883ff4fcd2fa696bec0455
Caleb Wallace <wallacekid1996@gmail.com>
2022-11-23 17:00:20 -0700

    server port

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.use(express.json())
 app.use(express.static(`${__dirname}/public`))
 
 
-app.listen(4001,
-   () => console.log(`server running on 4001`)
+app.listen(4000,
+   () => console.log(`server running on 4000`)
 )
